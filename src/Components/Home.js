e4a9aec7e3744fc2a93d1b907e51620b5236a610
gayathris2694@gmail.com <gayathris2694@gmail.com>
2024-05-04 08:14:37 +0530

    third commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import BannerBackground from "../Assets/Rectangle.png";
 import Golbe from "../Assets/earth.png";
 import Trusted from "../Assets/Trusted.png";
@@ -7,24 +6,32 @@ import Navbar from "./Navbar";
 
 const Home = () => {
   return (
-    <div className="homcontainere-banner-">
+    <div>
+      <Navbar />
       <div className="home-bannerImage-container">
-        <img src={BannerBackground} alt="" />
-        <div className="home-globe">
-          <img src={Golbe} alt="" />
-        </div>
-        <div className="home-container">
-          <Navbar />
-          <div className="home-text-section">
-            <h1 className="primary-heading">
-              Reach the Heart of your Target Audience
-            </h1>
-            <p className="primary-text">Book a free 30-Minute Strategy chat</p>
-            <button className="secondary-button">Book Now</button>
+        <img src={BannerBackground} alt="" className="img-fluid" />
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col">
+              <div className="home-globe">
+                <img src={Golbe} alt="" className="img-fluid" />
+              </div>
+              <div className="home-container">
+                <div className="home-text-section">
+                  <h1 className="primary-heading">
+                    Reach the Heart of your Target Audience
+                  </h1>
+                  <p className="primary-text">
+                    Book a free 30-Minute Strategy chat
+                  </p>
+                  <button className="secondary-button">Book Now</button>
+                </div>
+              </div>
+              <div className="home-Trusted">
+                <img src={Trusted} alt="" className="img-fluid" />
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="home-Trusted">
-          <img src={Trusted} alt="" />
         </div>
       </div>
     </div>
--- src/Components/Services.js
+++ src/Components/Services.js
@@ -4,28 +4,25 @@ import services from "../Assets/services.png";
 const Services = () => {
   return (
     <div className="services-container">
-      <br />
-      <br />
-      <br />
-
-      <h2 className="services-heading">Services</h2>
-      <br />
-      <h1>What We Offer</h1>
-      <p className="services-text">
-        We engage in integrated digital marketing strategies to ensure you've
-        got all your bases covered.
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-      </p>
-      <div className="services-img">
-        <img src={services} alt="" />
-        <br />
-        <br />
-        <br />
-        <br />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <h2 className="services-heading">Services</h2>
+            <br />
+            <h1>What We Offer</h1>
+            <p className="services-text">
+              We engage in integrated digital marketing strategies to ensure
+              you've got all your bases covered.
+            </p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+            <div className="services-img">
+              <img src={services} alt="" className="img-fluid" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
